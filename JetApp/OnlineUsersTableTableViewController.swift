3588a64d2e712007768bc089434a0ebf2f8f645b
Jet van den Berg <31651713+jetvdberg@users.noreply.github.com>
2018-01-09 17:00:35 +0100

    Update OnlineUsersTableTableViewController.swift

--- JetApp/OnlineUsersTableTableViewController.swift
+++ JetApp/OnlineUsersTableTableViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Jet van den Berg on 15-12-17.
 //  Copyright © 2017 Jet van den Berg. All rights reserved.
 //
-//  This file is not made by me, but used as base for the online users scene, gained from another project called 'Grocr'. Authors are: Ray Wenderlich, David East and Attila Hegedüs. https://www.raywenderlich.com/139322/firebase-tutorial-getting-started-2
+//  This file is not completely made by me, but used as base for the online users scene, gained from another project called 'Grocr'. Authors are: Ray Wenderlich, David East and Attila Hegedüs. https://www.raywenderlich.com/139322/firebase-tutorial-getting-started-2
 //
 
 import UIKit
@@ -14,16 +14,17 @@ import FirebaseDatabase
 
 class OnlineUsersTableViewController: UITableViewController {
     
-    // Constants
-    let userCell = "UserCell"
-    
     // Properties
+
+    let userCell = "UserCell"
     var currentUsers: [String] = []
-    let usersRef = Database.database().reference(withPath: "online")
+    let usersRef = Database.database().reference(withPath: "online-users")
     
     // UIViewController Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Checks if a user is online, adds user to Firebase
         usersRef.observe(.childAdded, with: { snap in
             guard let email = snap.value as? String else { return }
             self.currentUsers.append(email)
@@ -32,6 +33,7 @@ class OnlineUsersTableViewController: UITableViewController {
             self.tableView.insertRows(at: [indexPath], with: .top)
         })
         
+        // Checks if a user is offline, removes user from Firebase
         usersRef.observe(.childRemoved, with: { snap in
             guard let emailToFind = snap.value as? String else { return }
             for (index, email) in self.currentUsers.enumerated() {
@@ -45,11 +47,12 @@ class OnlineUsersTableViewController: UITableViewController {
     }
     
     // UITableView Delegate methods
-    
+    // Returns amount of online users
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return currentUsers.count
     }
     
+    // Fills in cell with online user
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: userCell, for: indexPath)
         let onlineUserEmail = currentUsers[indexPath.row]
@@ -58,7 +61,7 @@ class OnlineUsersTableViewController: UITableViewController {
     }
     
     // Actions
-    
+    // Sign out button, ends whole process, sends back to sign in viewController
     @IBAction func signoutButtonPressed(_ sender: AnyObject) {
         dismiss(animated: true, completion: nil)
     }
